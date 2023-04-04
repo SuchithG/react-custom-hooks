3f0a9968ee85d29f6e75bab4bac29f9361a342c5
SuchithG <suchithgowda1995@gmail.com>
2023-04-04 23:35:14 +0530

    feat: :zap: configuring custom hooks

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const useCounter = () => {
-    const [counter, setCounter] = useState(0);
+const useCounter = (forwards = true) => {
+  const [counter, setCounter] = useState(0);
 
-    useEffect(() => {
-      const interval = setInterval(() => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwards) {
         setCounter((prevCounter) => prevCounter + 1);
-      }, 1000);
-  
-      return () => clearInterval(interval);
-    }, []);
-    
-    return counter;
+      }else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
 };
 
-export default useCounter
+export default useCounter;
